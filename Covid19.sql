d6317d0c05637f1c5aa829917bded91ef888ab5c
JuanPoumian <juanpoumian01@gmail.com>
2023-03-22 20:39:03 -0600

    Yes

---
+++ Covid19.sql
@@ -0,0 +1,160 @@
+ -- Covid 19 Data Exploration 
+
+ -- Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+Select *
+From Portofolio..CovidDeaths
+Where continent is not null 
+order by 3,4
+
+-- Select Data that we are going to be starting with
+
+Select Location, date, total_cases, new_cases, total_deaths, population
+From Portofolio..CovidDeaths
+Where continent is not null 
+order by 1,2
+
+-- Total Cases vs Total Deaths
+-- Shows the likelihood of dying if you contract covid in Mexico
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From Portofolio..CovidDeaths
+Where location like 'Mexico'
+and continent is not null 
+order by 1,2
+
+-- Total Cases vs Total Deaths
+-- Shows the likelihood of dying if you contract covid in United States
+
+Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+From Portofolio..CovidDeaths
+Where location like '%State%'
+and continent is not null 
+order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid in Mexico 
+
+Select Location, date, Population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
+From Portofolio..CovidDeaths
+Where location like 'Mexico'
+order by 1,2
+
+-- Total Cases vs Population
+-- Shows what percentage of population infected with Covid in United States
+
+Select Location, date, Population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
+From Portofolio..CovidDeaths
+Where location like '%State%'
+order by 1,2
+
+-- Countries with Highest Infection Rate compared to Population
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From Portofolio..CovidDeaths
+--Where location like 'Mexico'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+-- Countries with Highest Death Count per Population
+
+Select Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From Portofolio..CovidDeaths
+--Where location like 'Mexico'
+Where continent is not null 
+Group by Location
+order by TotalDeathCount desc
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing continents with the highest death count per population
+
+Select continent, MAX(Convert(int, Total_deaths)) as TotalDeathCount
+From Portofolio..CovidDeaths
+--Where location like 'Asia'
+Where continent is not null 
+Group by continent
+Order by TotalDeathCount desc
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Portofolio..CovidDeaths
+Where continent is not null 
+--Group By date
+order by 1,2
+
+-- MEXICO NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Portofolio..CovidDeaths
+Where location like 'Mexico'
+and continent is not null 
+--Group By date
+order by 1,2
+
+-- UNITED STATES NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From Portofolio..CovidDeaths
+Where location like '%State%'
+and continent is not null 
+--Group By date
+order by 1,2
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
+
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From Portofolio..CovidDeaths dea
+Join Portofolio..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+order by 2,3
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
+With PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From Portofolio..CovidDeaths dea
+Join Portofolio..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentageVaccinated
+From PopvsVac
+
+-- Using Temp Table to perform Calculation on Partition By in previous query
+
+DROP Table if exists #PercentPopulationVaccinated
+Create Table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+Location nvarchar(255),
+Date datetime,
+Population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+Insert into #PercentPopulationVaccinated
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From Portofolio..CovidDeaths dea
+Join Portofolio..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+Select *, (RollingPeopleVaccinated/Population)*100 as PercentageVaccinated
+From #PercentPopulationVaccinated
+
+
